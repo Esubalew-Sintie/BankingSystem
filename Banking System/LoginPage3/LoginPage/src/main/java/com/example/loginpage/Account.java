9b9ec3c499f407b0e29c08aef48e35ceb0b47a00
SOL1001 <132193247+SOL1001@users.noreply.github.com>
2023-06-16 00:16:41 +0300

    Update Account.java

--- Banking System/LoginPage3/LoginPage/src/main/java/com/example/loginpage/Account.java
+++ Banking System/LoginPage3/LoginPage/src/main/java/com/example/loginpage/Account.java
@@ -10,7 +10,7 @@ import javafx.beans.property.StringProperty;
         private String password;
         private String role;
 
-sfvfvfddddd
+sfvfvfddddddcfvvvvvvvvvvvvvvvvvvv
 
         public Account(String name, String id, String username, String password, String role) {
             this.name = name;
